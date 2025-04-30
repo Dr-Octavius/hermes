816508a7f6eb7a30e4c784295e79c5ef93cd42a0
CaspianMike-L <looweesing@gmail.com>
2025-04-30 16:46:54 +0800

    wip: modularise istio deployment code

---
+++ terraform/dev/modules/istio/istiod/main.tf
@@ -0,0 +1,13 @@
+#----------------------
+# main.tf
+# - Planned Resources
+#----------------------
+
+#------------------------
+# Planned Resources
+# - resource blocks only
+#------------------------
+# Istio Base Installation using Helm
+resource "helm_release" "istio_base" {
+
+}
